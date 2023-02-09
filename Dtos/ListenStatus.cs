14237c95b8a1fd14b46af1453055d1e459a41b32
Rodrigo Lins <rlinsdev@gmail.com>
2023-02-09 15:12:36 -0300

    DTOs - ListenStatus

---
+++ Dtos/ListenStatus.cs
@@ -0,0 +1,10 @@
+namespace AsyncProductAPI.Dtos
+{
+    public class ListenStatus
+    {
+        public string? RequestStatus {get; set;}
+        public string? EstimatedCompetitionTime { get; set; }
+        public string? ResourceURL { get; set; }
+        
+    }
+}
