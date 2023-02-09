ea094a21fc9d92ce92de5aeb2e0e3ca4b5cca0f5
Rodrigo Lins <rlinsdev@gmail.com>
2023-02-09 12:10:38 -0300

    Initialization

--- Program.cs
+++ Program.cs
@@ -1,4 +1,9 @@
+using AsyncProductAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(opt => opt.UseSqlite("Data Source=RequestDb.db"));
 
 var app = builder.Build();
 
