ca7f5a598b31fd28a5e390a0a6a3e5f364ba2d50
Rodrigo Lins <rlinsdev@gmail.com>
2023-02-09 11:57:06 -0300

    Model - listing request

---
+++ Models/ListingRequest.cs
@@ -0,0 +1,11 @@
+namespace AsyncProductAPI.Models
+{
+    public class ListingRequest
+    {
+        public int Id { get; set; }
+        public string? RequestBody { get; set; }
+        public string? EstimatedCompetitionTime { get; set; }
+        public string? RequestStatus { get; set; }
+        public string RequestId { get; set; } = Guid.NewGuid().ToString();
+    }    
+}
---
+++ README.md
@@ -0,0 +1,12 @@
+# Async Product API
+
+## Commands
+```Bash
+$ dotnet new webapi -minimal -n AsyncProductAPI
+$ dotnet add package Microsoft.EntityFrameworkCore.Design
+$ dotnet add package Microsoft.EntityFrameworkCore.Sqlite
+
+```
+
+## Links:
+* [GPT Chat](https://chat.openai.com/chat)
